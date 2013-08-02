4a04e419b98938d087143b77e6bde6c2d15f5249
Evgeniy Dolgikh <marcondream@gmail.com>
2013-08-02 11:54:16 +0300

    line directions

---
+++ src/ua/org/atsy/xo/Direction.java
@@ -0,0 +1,9 @@
+package ua.org.atsy.xo;/*
+Copyright (c) 2013 Evgeniy Dolgikh
+
+See the file LICENSE for copying permission.
+*/
+
+public enum Direction {
+    HORIZONTAL, VERTICAL, DIAGONAL_FIRST, DIAGONAL_SECOND
+}
