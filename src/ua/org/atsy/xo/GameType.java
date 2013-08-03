6225a36b140ead230dd7ef948039a72718de6606
Evgeniy Dolgikh <marcondream@gmail.com>
2013-08-03 21:27:57 +0300

    add game types enum

---
+++ src/ua/org/atsy/xo/GameType.java
@@ -0,0 +1,11 @@
+package ua.org.atsy.xo;/*
+Copyright (c) 2013 Evgeniy Dolgikh
+
+See the file LICENSE for copying permission.
+*/
+
+public enum GameType {
+    PVP, //Player vs Player
+    PVC, //Player vs CPU
+    NET  //Player vs Player via LAN
+}
