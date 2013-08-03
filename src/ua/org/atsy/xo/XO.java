070e4513470a359b1b3e8aa9fec03aa77d2743c1
Evgeniy Dolgikh <marcondream@gmail.com>
2013-08-03 18:11:56 +0300

    fix main

--- src/ua/org/atsy/xo/XO.java
+++ src/ua/org/atsy/xo/XO.java
@@ -1,9 +1,9 @@
-/*
-Copyright (c) 2013 Evgeniy Dolgikh
-
-See the file LICENSE for copying permission.
-*/
 package ua.org.atsy.xo;
 
 public class XO {
+
+    public static void main(String[] args) {
+        Field field = new Field();
+        field.printField();
+    }
 }
