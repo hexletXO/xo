6c7d4fe0d6eefa960b681c5f787e3ba6630e9895
Evgeniy Dolgikh <marcondream@gmail.com>
2013-08-03 20:40:24 +0300

    add game stats

---
+++ src/ua/org/atsy/xo/GameStats.java
@@ -0,0 +1,39 @@
+/*
+Copyright (c) 2013 Evgeniy Dolgikh
+
+See the file LICENSE for copying permission.
+*/
+package ua.org.atsy.xo;
+
+public class GameStats {
+    private int gamesFinished = 0;
+    private int turn = 1;
+    private int[] scores = new int[2];
+
+    public int getGamesFinished() {
+        return gamesFinished;
+    }
+
+    public int getTurn() {
+        return turn;
+    }
+
+    public int[] getScores() {
+        return scores;
+    }
+
+    public void increaseTurn() {
+        turn++;
+    }
+    public void resetTurns() {
+        turn = 1;
+    }
+    public void resetScores() {
+        scores[0] = 0;
+        scores[1] = 1;
+    }
+    public void increaseScores(int playerNum) {
+        scores[playerNum]++;
+    }
+
+}
