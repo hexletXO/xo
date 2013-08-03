de0355f8a43cba18cebb1f5505efce8c1b38d3e7
Evgeniy Dolgikh <marcondream@gmail.com>
2013-08-03 18:58:05 +0300

    add ask question add mainLoop

---
+++ src/ua/org/atsy/xo/XO.java
@@ -0,0 +1,27 @@
+/*
+Copyright (c) 2013 Evgeniy Dolgikh
+
+See the file LICENSE for copying permission.
+*/
+package ua.org.atsy.xo;
+import java.util.Scanner;
+
+public class XO {
+    private boolean running = true;
+    Scanner in;
+    public XO() {
+        in = new Scanner(System.in);
+
+    }
+    private String askUser(String question) {
+        System.out.print(question + ": ");
+        return  in.next();
+    }
+    public void mainLoop() {
+        while(running) {
+            if(askUser("Exit? y/n").equals("y")) {
+                running = false;
+            }
+        }
+    }
+}
